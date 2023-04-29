2dd0fefadbffb7e2894e79e9b40c7d21a8efc625
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-29 06:42:25 -0300

    Script file for redisinsight job

---
+++ scripts/start/redisinsight.sh
@@ -0,0 +1,11 @@
+cd $HOME/docker-compose.yml
+
+if [ $START_REDISINSIGHT == 'true' ]
+then
+  echo "REDISINSIGHT_CONTAINER_NAME=workspeace-redisinsight" >> .env
+  echo "REDISINSIGHT_RITRUSTEDORIGINS=https://redisinsight.$SERVER_SSH_HOST" >> .env
+  echo "REDISINSIGHT_RESTART_POLICY=unless-stopped" >> .env
+  docker compose up redisinsight -d
+else
+  docker compose stop redisinsight
+fi
