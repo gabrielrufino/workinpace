a3b674778f95d1522a914ce0dc842ab02a85310b
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-29 06:58:14 -0300

    Script file for traefik job

---
+++ scripts/start/traefik.sh
@@ -0,0 +1,13 @@
+cd $HOME/docker-compose.yml
+
+ENCRYPTED_TRAEFIK_PASSWORD=$(htpasswd -nbs $TRAEFIK_DASHBOARD_USERNAME $TRAEFIK_DASHBOARD_PASSWORD | cut -d ':' -f 2)
+
+echo "TRAEFIK_CONTAINER_NAME=workspeace-traefik" >> .env
+echo "TRAEFIK_BASE_HOST=$DOMAIN" >> .env
+echo "TRAEFIK_ACME_EMAIL=$ACME_EMAIL" >> .env
+echo "TRAEFIK_DASHBOARD_USERNAME=$TRAEFIK_DASHBOARD_USERNAME" >> .env
+echo "TRAEFIK_DASHBOARD_PASSWORD=$ENCRYPTED_TRAEFIK_PASSWORD" >> .env
+echo "TRAEFIK_RESTART_POLICY=always" >> .env
+
+docker compose up traefik -d
+ufw-docker allow workspeace-traefik
