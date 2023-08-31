373c61effb58b4cfe1dfe93f6f702be7c272f6f6
Gabriel Rufino <contato@gabrielrufino.com>
2023-08-31 10:45:43 -0300

    New environment variable: REDIS_PASSWORD

--- scripts/start/redis.sh
+++ scripts/start/redis.sh
@@ -3,6 +3,7 @@ cd $HOME/docker-compose.yml
 if [ $START_REDIS == 'true' ]
 then
   echo "REDIS_CONTAINER_NAME=workinpace-redis" >> .env
+  echo "REDIS_PASSWORD=$REDIS_PASSWORD" >> .env
   echo "REDIS_RESTART_POLICY=unless-stopped" >> .env
   docker compose up redis -d
 else
